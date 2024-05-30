96ce2934e2ff2eff0145604e4f2f16cf2de4e753
SATHISH-1718 <iamsathish1718@gmail.com>
2024-05-30 22:20:57 +0530

    product table update

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,27 +1,16 @@
 import "../App.css";
 import ProductCard from './ProductCard';
 import ProductTable from './ProductTable';
-import { Link } from 'react-router-dom';
+
 
 function Product() {
     return (
         <>
-        <br />
-        <br />
             <h1 className="text-center">Product</h1>
             <hr />
             <ProductCard />
             <div className="table-responsive">
-                <div className="row d-flex justify-content-center">
-                    <div className="col-12 d-flex justify-content-end mb-2">
-                        <Link to="/dashboard/products/addproduct" className="active">
-                            <button type="button" className="btn btn-primary" id="addbtn">Add product</button>
-                        </Link>
-                    </div>
-                    <div className="col-12">
-                        <ProductTable />
-                    </div>
-                </div>
+                <ProductTable />   
             </div>
         </>
     );
